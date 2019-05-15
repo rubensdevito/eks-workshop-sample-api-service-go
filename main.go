fadbbbe662b3a5000f36b7d08909666161aed610
rubensdevito <rubensdevito@gmail.com>
2019-05-15 10:53:17 -0300

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World V2"}
+		res := &response{Message: "Hello World V3"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
